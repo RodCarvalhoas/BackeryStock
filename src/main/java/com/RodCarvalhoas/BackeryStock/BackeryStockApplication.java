34bf7a166c6a682d11e898a8709f5c3787352742
RodCarvalhoas <sutil_rodrigo@hotmail.com>
2023-06-19 20:09:18 -0300

    Inicio do projeto

---
+++ src/main/java/com/RodCarvalhoas/BackeryStock/BackeryStockApplication.java
@@ -0,0 +1,13 @@
+package com.RodCarvalhoas.BackeryStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  BackeryStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackeryStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RodCarvalhoas/BackeryStock/BackeryStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RodCarvalhoas.BackeryStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackeryStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
